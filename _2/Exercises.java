9af2a309e425867df36417b54830e99b32b418ff
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-03-30 18:46:17 +0200

    Es_OOP2

---
+++ _2/Exercises.java
@@ -0,0 +1,110 @@
+package Esercizi_OOP2._2;
+
+import java.util.*;
+
+public class Exercises {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: Follow the steps below
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+        // 1a. Create a map of integers to strings
+        Map<Integer, String> myFirstMap = new HashMap<>();
+        // 1b. Add 3 elements to the map .put
+        myFirstMap.put(1,"One");
+        myFirstMap.put(2,"Two");
+        myFirstMap.put(3,"Three");
+        // 1c. Print the map
+        System.out.println(myFirstMap);
+        // 1d. Access an element in the map with .get
+        System.out.println(myFirstMap.get(1));
+        // 1e. Remove an element from the map .remove
+        myFirstMap.remove(1);
+        // 1f. Print the map again to confirm the element was removed
+        System.out.println(myFirstMap);
+    }
+
+    /**
+     *  2: Follow the instructions in the code
+     */
+    private static void exercise2() {
+        System.out.println("Exercise 2: ");
+        Map<String, Double> menuCostsInEuro = new HashMap<>();
+
+        menuCostsInEuro.put("Fries", 3.50);
+        menuCostsInEuro.put("Pizza", 11.80);
+        menuCostsInEuro.put("Burger", 8.50);
+
+        String mostExpensiveMenuItemName = null;
+
+        // 2a. Loop over the map entries with menuCostsInEuro.entrySet()
+        //     and set out the most expensive items name
+        //     use for(Map.Entry<String, Double> entry : menuCostsInEuro.entrySet()) and entry.getKey and entry.getValue
+        //     to do so
+
+        List<Double>mapCosts=new ArrayList<>();
+        //Forse esisteva un modo più compatto di farlo, alle 2 di notte non mi è venuto in mente
+
+        for (Map.Entry<String,Double>costs :menuCostsInEuro.entrySet()){
+
+            mapCosts.add(costs.getValue());
+
+        }
+
+        for (Map.Entry<String,Double> mapElement :menuCostsInEuro.entrySet()){
+
+
+            if(mapElement.getValue().equals(Collections.max(mapCosts))){
+
+                mostExpensiveMenuItemName=mapElement.getKey();
+
+            }
+        }
+
+        if (mostExpensiveMenuItemName.equals("Pizza")) {
+            System.out.println("2a. Correct!");
+        } else {
+            System.out.println("2a. Incorrect!");
+        }
+    }
+
+    /**
+     * 3: Follow the instructions in the code
+     */
+    private static void exercise3() {
+        System.out.println("Exercise 3: ");
+
+        Map<String, Integer> fruitWeights = new HashMap<>();
+
+        fruitWeights.put("Apple", 5);
+        fruitWeights.put("Banana", 3);
+        fruitWeights.put("Orange", 8);
+        fruitWeights.put("Kiwi", 1);
+        fruitWeights.put("Mango", 4);
+        fruitWeights.put("Grapes", 8);
+        fruitWeights.put("Peach", 5);
+        fruitWeights.put("Pineapple", 1);
+
+        int totalWeight = 0;
+        // 3a. Use a for loop with fruitWeights.values() and sum up the total weight
+
+        for (Map.Entry<String,Integer> weights : fruitWeights.entrySet()){
+
+            totalWeight+=weights.getValue();
+
+        }
+
+
+        if (totalWeight == fruitWeights.values().stream().mapToInt(Integer::intValue).sum()) {//Immagino questa sia la maniera furba di farlo
+            System.out.println("3. Correct!");
+        } else {
+            System.out.println("3. Incorrect!");
+        }
+    }
+}
